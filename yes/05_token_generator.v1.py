68aca05089ec9b5db1d9f281a7782b4f12de8716
rafaelanggawijaya <124117040+rafaelanggawijaya@users.noreply.github.com>
2023-04-19 16:08:06 +1200

    update
    just adding new compnent

---
+++ yes/05_token_generator.v1.py
@@ -0,0 +1,14 @@
+"""Component 3 Token Generator
+ Generates random tokens (Unicorn, Zebra, Horse, Donkey
+ created by Rafael Anggawijaya"""
+
+# imports random function
+import random
+
+tokens = ["unicorn", "zebra", "horse", "donkey"]
+
+# test loop for generating random tokens
+
+for item in range(20):
+    token = random.choice(tokens)
+    print(token, end='\t')  # easier to screenshot
