3fa0d864241830f04d9b4cee9cfc5ade40d768b6
rafaelanggawijaya <124117040+rafaelanggawijaya@users.noreply.github.com>
2023-04-21 14:50:27 +1200

    update
    added token generator

---
+++ yes/05_token_generator.v3.py
@@ -0,0 +1,38 @@
+"""Component 3 Token Generator v3
+ Generates random tokens (Unicorn, Zebra, Horse, Donkey
+ Update: previous code had the house lose most of the time
+        unicorns come out 10%
+        zebra come out 20%
+        horse come out 20%
+        donkey come out 50%
+of the time
+ created by Rafael Anggawijaya"""
+
+# imports random function
+import random
+
+tokens = ["unicorn", "zebra", "zebra", "horse", "horse", "donkey", "donkey", "donkey", "donkey", "donkey", ]
+
+# Balance for test
+
+STARTING_BALANCE = 100
+balance = STARTING_BALANCE
+# test loop for generating random tokens
+
+for item in range(20):
+    token = random.choice(tokens)
+    print(token, end='\t')  # easier to screenshot
+
+    # changes balance
+
+    if token == "unicorn":
+        balance += 4
+    elif token == "donkey":
+        balance -= 1
+    else:
+        balance -= 0.5
+
+
+# output
+print(f"\nStarting balance:${STARTING_BALANCE:.2f}")
+print(f"balance:${balance:.2f}")
---
+++ yes/05_token_generator.v4.py
@@ -0,0 +1,46 @@
+"""Component 3 Token Generator v4
+ Generates random tokens (Unicorn, Zebra, Horse, Donkey)
+ Update: percentages are changed
+        unicorns come out 5%
+        zebra come out 32.5%
+        horse come out 32.5%
+        donkey come out 30%
+of the time.
+ created by Rafael Anggawijaya"""
+
+# imports random function
+import random
+
+
+# Balance for test
+
+STARTING_BALANCE = 100
+balance = STARTING_BALANCE
+# test loop for generating random tokens
+
+for item in range(10):
+    number = random.randint(1, 100)
+    if number <= 5:
+        token = "unicorn"
+    elif number <= 35:
+        token = "donkey"
+    else:
+        if number % 2 == 0:
+            token = "zebra"
+        else:
+            token = "horse"
+    print(token, end='\t')  # easier to screenshot
+
+    # changes balance
+
+    if token == "unicorn":
+        balance += 4
+    elif token == "donkey":
+        balance -= 1
+    else:
+        balance -= 0.5
+
+
+# output
+print(f"\nStarting balance:${STARTING_BALANCE:.2f}")
+print(f"balance:${balance:.2f}")
